066adfb3885592b3371a57a8ced53f0da4e2a140
yoonsangwon0312 <tkddnjs0312@gmail.com>
2024-02-19 11:38:11 +0900

    logger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { LoggerLibrary } from './common/logger.library';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: new LoggerLibrary(),
+  });
+  
   await app.listen(3000);
 }
 bootstrap();
